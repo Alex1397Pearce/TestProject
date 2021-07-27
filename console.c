9543c827b81cc6ce2df3f80d1c4b6e2949335abc
Alex1397Pearce <alx13mikh@gmail.com>
2021-07-27 16:47:48 +0000

    version1.1

---
+++ console.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+//#include <iostream>
+//using namespace std:
+
+int main(){
+//	cout >> "Hello World!";
+	printf("Hello World \n");
+	int Sum;
+	Sum = 12;
+	printf("Sum = %d \n",Sum);
+	return 0;
+}
