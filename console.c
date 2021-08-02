12347c70e256cdfc90c7bbf9ffbe21ebba86b5d8
Alex1397Pearce <alx13mikh@gmail.com>
2021-08-02 20:23:13 +0000

    Auto-commit by Jenkins - 20:23-2021.08.02

--- console.c
+++ console.c
@@ -10,6 +10,7 @@ int main(){
 	Sum = 12;
 	printf("Sum = %d \n",Sum);
 <<<<<<< HEAD
+<<<<<<< HEAD
 	int a = 20, b = 30;
 	//int res = Calc(a, b);
 	//printf("Result function = %d \n", res);
@@ -19,16 +20,21 @@ int main(){
 //	int res = Calc(a, b);
 //	printf("Result function = %d \n", res);
 >>>>>>> bAlexPearce
+=======
+	int a = 20, b = 30;
+	int res = Calc(a, b);
+	printf("Result function = %d \n", res);
+>>>>>>> bAlexPearce
 	printf("--------Build complete------------ \n");
 	return 0;
 }
 
-//int Calc(int FirstCount, int SecondCount){
-//	int Sum = FirstCount + SecondCount;
-//	return Sum;
-//}
+int Calc(int FirstCount, int SecondCount){
+	int Sum = FirstCount + SecondCount;
+	return Sum;
+}
 
-//int Print(){
-//	printf("Da poshli vi vse nahui \n");
-//	return 0;
-//}
+int Print(){
+	printf("Da poshli vi vse nahui \n");
+	return 0;
+}
