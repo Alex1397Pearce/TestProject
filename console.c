422d8c7dc2142afcd4b1411a93452d44347ec686
Alex1397Pearce <alx13mikh@gmail.com>
2021-08-02 19:52:31 +0000

    Version 1.7

--- console.c
+++ console.c
@@ -9,19 +9,19 @@ int main(){
 	int Sum;
 	Sum = 12;
 	printf("Sum = %d \n",Sum);
-//	int a = 20, b = 30;
-//	int res = Calc(a, b);
-//	printf("Result function = %d \n", res);
+	int a = 20, b = 30;
+	int res = Calc(a, b);
+	printf("Result function = %d \n", res);
 	printf("--------Build complete------------ \n");
 	return 0;
 }
 
-//int Calc(int FirstCount, int SecondCount){
-//	int Sum = FirstCount + SecondCount;
-//	return Sum;
-//}
+int Calc(int FirstCount, int SecondCount){
+	int Sum = FirstCount + SecondCount;
+	return Sum;
+}
 
-//int Print(){
-//	printf("Da poshli vi vse nahui \n");
-//	return 0;
-//}
+int Print(){
+	printf("Da poshli vi vse nahui \n");
+	return 0;
+}
